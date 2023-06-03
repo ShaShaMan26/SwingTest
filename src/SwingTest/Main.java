b0a647356d5b18f7fa7a41076092ec5107ae67b6
Cristiano Porretta <shashaman26@icloud.com>
2023-06-03 02:57:17 -0400

    added-gitignore

--- src/SwingTest/Main.java
+++ src/SwingTest/Main.java
@@ -3,7 +3,6 @@ package SwingTest;
 public class Main {
     public static GameInstance gameInstance = new GameInstance();
     public static void main(String[] args) {
-
         MetaActionListener metaActionListener = new MetaActionListener(gameInstance);
         gameInstance.gameWindow.addKeyListener(metaActionListener);
 
